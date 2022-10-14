de6b48d000f34a79173684909ad5fab96e71951c
John Patrick <jpatrickzephyr@gmail.com>
2022-10-14 14:43:25 +0800

    feat:add push notifications

--- door_lock_system.ino
+++ door_lock_system.ino
@@ -1,3 +1,6 @@
+#define BLYNK_PRINT Serial
+#include <BlynkSimpleEsp8266.h>
+#define BLYNK_TEMPLATE_ID "TMPLRja1ACw4"
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <SPI.h>
@@ -11,17 +14,23 @@
 #define BUZZER D8 
 #define SERVO D10
 #define LED_R D9
+#define LED_G D0
+#define BLYNK_AUTH_TOKEN "_1STTWJUCtdFvsr76dtSzioqTSh0GW5E" //Enter your blynk auth token
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 Servo sg90;
+char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "ZTE_2.4G_xqiREP";//Enter your WIFI name
 char pass[] = "E6YDAka5";//Enter your WIFI password
 String tagUID = "F3 72 37 30";
-int counter = 0;
-
+int counter = 3;
+int invalidRetries = 0;
+BLYNK_WRITE(V0) {
+  digitalWrite(LED_G, param.asInt());
+}
 void setup() {
- 
-	Serial.begin(115200);		// Initialize serial communications with the PC
+  
+	Serial.begin(9600);		// Initialize serial communications with the PC
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
@@ -35,16 +44,19 @@ void setup() {
   lcd.begin();
   lcd.backlight();
   pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   noTone(BUZZER);
   sg90.attach(SERVO);
   sg90.write(0);
 	SPI.begin();			
 	mfrc522.PCD_Init();		
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
 	lcd.clear();
 }
 
 void loop() {
+    Blynk.run();
     WiFiClient client;
     HTTPClient http;
     lcd.setCursor(0, 0);
@@ -76,11 +88,13 @@ void loop() {
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print("Access Granted");
+      digitalWrite(LED_G, HIGH);
       lcd.setCursor(0, 1);
       lcd.print("Unlocking Door");
       sg90.write(160);
-      tone(BUZZER, 100);
+      tone(BUZZER, 400);
       delay(2000);
+       digitalWrite(LED_G, LOW);
       noTone(BUZZER);
       sg90.write(0);
       lcd.clear();
@@ -89,21 +103,29 @@ void loop() {
     else
     {
       // If UID of tag is not matched.
-      counter++;
+      counter--;
+      invalidRetries++;
       lcd.clear();
-      lcd.print(counter);
+      lcd.setCursor(0, 0);
+      lcd.print("Access denied");
+      lcd.setCursor(0, 1);
+      lcd.print(String(counter) + " attempts left");
       digitalWrite(LED_R, HIGH);
       tone(BUZZER, 300);
       delay(2000);
       digitalWrite(LED_R, LOW);
       noTone(BUZZER);
       lcd.clear();
-      if(counter==3){
-         http.begin(client,"https://maker.ifttt.com/trigger/Demo/json/with/key/mcdd7ioVhfAmj0_Hp6fQSQ04Y9HXAfyiuqitVysx0Xw");
-         http.GET();
-         http.end();
-         counter =0;
-   
+      Serial.print(counter);
+      if(counter==0){
+        counter =3;
       }
-    }
+      if(invalidRetries==3){
+        http.begin(client,"http://maker.ifttt.com/trigger/entry/json/with/key/mcdd7ioVhfAmj0_Hp6fQSQ04Y9HXAfyiuqitVysx0Xw");
+        http.GET();
+        http.end();
+        invalidRetries=0;
+      }
+   }
+   
 }
